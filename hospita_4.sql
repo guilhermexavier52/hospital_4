fd83912d30f07103d44093ac72e289540cf2d6d0
Guilherme Xavier <125416199+guilhermexavier52@users.noreply.github.com>
2023-11-15 08:30:10 -0300

    Add files via upload

---
+++ hospita_4.sql
@@ -0,0 +1,14 @@
+use bd_hospital;
+
+alter table tbl_medico add atividade varchar(20);
+
+update tbl_medico set atividade = 'ativo' where id_medico = 1;
+update tbl_medico set atividade = 'ativo' where id_medico = 2;
+update tbl_medico set atividade = 'ativo' where id_medico = 3;
+update tbl_medico set atividade = 'ativo' where id_medico = 4;
+update tbl_medico set atividade = 'ativo' where id_medico = 5;
+update tbl_medico set atividade = 'ativo' where id_medico = 6;
+update tbl_medico set atividade = 'ativo' where id_medico = 7;
+update tbl_medico set atividade = 'ativo' where id_medico = 8;
+update tbl_medico set atividade = 'ativo' where id_medico = 9;
+update tbl_medico set atividade = 'ativo' where id_medico = 10;
